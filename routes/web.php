05e0880dd22805f16c8b0b3b8a062af2d9a7c5ef
walker <walker.leo@unemat.br>
2026-04-30 16:02:58 -0400

    Criando rota de assets.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Livewire\AssetManager;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/', 'welcome');
+
+Route::view('dashboard', 'dashboard')
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
+
+Route::view('profile', 'profile')
+    ->middleware(['auth'])
+    ->name('profile');
+
+Route::get('/assets', AssetManager::class)
+    ->middleware(['auth', 'verified'])
+    ->name('assets.index');
+
+require __DIR__.'/auth.php';
