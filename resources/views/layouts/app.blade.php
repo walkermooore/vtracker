37d4237950313e59fa9a4833427edc387f093736
Walker <walkermoore4408@gmail.com>
2026-05-02 00:35:12 -0400

    Inclui a biblioteca Chart.js no cabeçalho do sistema para permitir a renderização de gráficos em toda a aplicação.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
+        <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
