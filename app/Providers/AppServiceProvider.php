1d77aaf2555410f89b940ee58b0015d532fe4432
Walker <walkermoore4408@gmail.com>
2026-05-02 00:08:26 -0400

    Adicionando salvamento de log automático

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Models\Vulnerability;
+use App\Observers\VulnerabilityObserver;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Vulnerability::observe(VulnerabilityObserver::class);
     }
 }
